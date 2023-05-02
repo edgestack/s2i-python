72b4152cf60537aee4f1f1a91ad12fac373871e6
Jian Li <pyguni@gmail.com>
2023-05-01 22:11:33 -0400

    Support multi-arch build

---
+++ multiarch_build_27.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -ex
+set -o pipefail
+
+TAG=${TAG:-v3.2.0}
+REPO=${REPO:-registry.gitlab.com/sonaproject/s2i-python}
+
+# support other container tools. e.g. podman
+CONTAINER_CLI=${CONTAINER_CLI:-docker}
+CONTAINER_BUILDER=${CONTAINER_BUILDER:-"buildx build"}
+
+# supported platforms
+PLATFORMS=linux/amd64,linux/arm64
+
+# shellcheck disable=SC2086 # inteneded splitting of CONTAINER_BUILDER
+${CONTAINER_CLI} ${CONTAINER_BUILDER} \
+  --platform ${PLATFORMS} \
+  --push \
+  -f 2.7/Dockerfile.rhel8 \
+  -t "${REPO}"/python-27-rhel8:"${TAG}" .
+
