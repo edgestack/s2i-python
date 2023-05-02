4b87c94811b7dd983d020aef1b2cc5752a009269
Jian Li <pyguni@gmail.com>
2023-05-02 11:08:51 -0400

    Fix: import the missing sysinfo dependencies #3

--- examples/django-system-info-app/project/urls.py
+++ examples/django-system-info-app/project/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import path
 from django_sysinfo.views import sysinfo, version, check, echo
 
 urlpatterns = [
